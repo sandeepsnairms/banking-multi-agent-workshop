bb044f9b2a88a6cab0de6e4114324a0fd9deae74
Sandeep Nair <nair.sandeep@microsoft.com>
2025-03-25 19:39:03 +0530

    Fixing Frontend deployment script

--- csharp/azd-hooks/postdeploy.ps1
+++ csharp/azd-hooks/postdeploy.ps1
@@ -1,11 +1,59 @@
 Param(
+    [parameter(Mandatory=$false)][string]$webAppName=$env:WEB_APP_NAME,
+    [parameter(Mandatory=$false)][string]$resourceGroup=$env:RG_NAME,
+	[parameter(Mandatory=$false)][string]$webAppUrl=$env:FRONTENDPOINT_URL,
     [parameter(Mandatory=$false)][string]$apiUrl=$env:SERVICE_ChatAPI_ENDPOINT_URL
 )
 
 # Check if API URL is provided
 if (-not $apiUrl) {
-    Write-Host "Error: API URL is required. Usage: .\postdeploy.ps1 -apiUrl 'https://your-api.com/resource'"
+    Write-Host "Error: API URL is required."
     exit 1
+}
+
+# Function to upload frontend app
+function Build-And-Deploy-Frontend {
+    param (
+        [string]$frontendPath = "..\frontend",
+        [string]$webAppName,
+        [string]$resourceGroup,
+        [string]$buildOutput = "dist\multi-agent-app"  # Change if the output folder is different
+    )
+
+    if (-not $webAppName -or -not $resourceGroup) {
+        Write-Host "Error: Web App name and Resource Group are required."
+        return
+    }
+		
+    # Switching to frontend directory
+    Set-Location -Path $frontendPath
+	
+	# Setting ChatServiceWebApi URL in frontend app"
+	$envContent = "export const environment = { apiUrl: '$apiUrl/' };"
+	$envFilePath = "$frontendPath\src\environments\environment.prod.ts"  # Adjust the path accordingly
+
+	$envContent | Out-File -FilePath $envFilePath -Encoding utf8 -Force
+
+	
+    Write-Host "Installing dependencies..."
+    npm install
+	
+    Write-Host "Building frontend..."
+	ng build --configuration=production
+
+    
+    $zipFile = "..\frontend\dist\app.zip"
+	
+	#Delete the existing zip file if it exists
+	 if (Test-Path $zipFile) {
+		Remove-Item $zipFile -Force
+	 }
+	
+	Write-Host "`r`Compressing $frontendPath\$buildOutput build output to $zipFile"	
+    Compress-Archive -Path "$frontendPath\$buildOutput\browser\*" -DestinationPath $zipFile -Force
+
+	Write-Host "`r`Deploying to Azure Web App..."
+	az webapp deploy --resource-group $resourceGroup --name $webAppName --src-path $zipFile --type zip --only-show-errors
 }
 
 # Function to send data to API
@@ -16,9 +64,10 @@ function Send-Data($jsonFilePath, $endpoint) {
     }
     
     $apiEndpointUrl = "$apiUrl/$endpoint"
+    
     # Read the JSON file as UTF-8 and parse it
-	$jsonContent = Get-Content -Path $jsonFilePath -Raw -Encoding UTF8
-	$jsonArray = $jsonContent | ConvertFrom-Json
+    $jsonContent = Get-Content -Path $jsonFilePath -Raw -Encoding UTF8
+    $jsonArray = $jsonContent | ConvertFrom-Json
     
     foreach ($item in $jsonArray) {
         try {
@@ -27,24 +76,26 @@ function Send-Data($jsonFilePath, $endpoint) {
         } catch {
             Write-Output "Error: $_"
         }
-		Start-Sleep -Milliseconds 500  # Sleeps for 0.5 seconds
+        Start-Sleep -Milliseconds 500  # Sleep for 0.5 seconds
     }
 }
 
 # Ask user if they want to add dummy data
-$dummyDataResponse = Read-Host "Do you want to add some dummy data for testing? (yes/no)"
+$dummyDataResponse = Read-Host "`r`Do you want to add some dummy data for testing? (yes/no)"
 if ($dummyDataResponse -match "^(yes|y)$") {
     Write-Host "Adding dummy data..."
     # Load dummy data
-	Send-Data "./data/UserData.json" "userdata"
+    Send-Data "./data/UserData.json" "userdata"
 	Send-Data "./data/AccountsData.json" "accountdata"
 	Send-Data "./data/OffersData.json" "offerdata"
-	
-	Write-Host "Data load completed."
-} else {
-    Write-Host "Skipping dummy data addition."
 }
 
-Start-Sleep -Milliseconds 2000  # Sleeps for 2 seconds
-
-
+# Ask user if they want to deploy frontend
+$dummyDataResponse = Read-Host "`r`Do you want to deploy the frontend app? (yes/no)"
+if ($dummyDataResponse -match "^(yes|y)$") {
+    Write-Host "***FRONTEND APP deployment started!***"
+	# Deploy frontend
+	Build-And-Deploy-Frontend -webAppName $webAppName -resourceGroup $resourceGroup
+	
+	Write-Host "`r`Deployment complete. You can visit your app at : $webAppUrl"
+}
