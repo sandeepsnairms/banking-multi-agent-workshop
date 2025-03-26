cb17d066c589b148d8c81c8c998d32ae616441fd
Mark Brown <mjbrown@microsoft.com>
2025-03-25 18:02:50 -0700

    update starter solution

--- csharp/src/Common/Models/Banking/BankAccount.cs
+++ csharp/src/Common/Models/Banking/BankAccount.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace MultiAgentCopilot.Common.Models.Banking
 {
-    public class BankUser
+    public class BankAccount
     {
         public required string Id { get; set; } = string.Empty;
         public required string TenantId { get; set; } = string.Empty;
         public required string Name { get; set; } = string.Empty;
-        public required string Email { get; set; }
-        public required string PhoneNumber { get; set; }
-        public required Dictionary<string, object> Attributes { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public required AccountType AccountType { get; set; }
+        public long Balance { get; set; }
+        public long Limit { get; set; }
+        public int InterestRate { get; set; }
+        public required string ShortDescription { get; set; }
     }
-
 }
