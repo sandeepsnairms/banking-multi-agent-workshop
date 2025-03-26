c822dce08ffb4e101a47a4cde6c37129f4e568f4
Sandeep Nair <nair.sandeep@microsoft.com>
2025-03-26 09:12:03 +0530

    Adding missing ServiceRequestType

---
+++ csharp/src/Common/Models/Banking/ServiceRequestType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiAgentCopilot.Common.Models.Banking
+{
+    public enum ServiceRequestType
+    {
+        Complaint = 0,
+        FundTransfer = 1,
+        Fulfilment = 2,
+        TeleBankerCallBack = 3
+    }
+}
