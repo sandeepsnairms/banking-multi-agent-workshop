b1ec651d6171475012d5d9a4bb913bd00b1c5fb1
Sandeep Nair <nair.sandeep@microsoft.com>
2025-04-16 17:43:18 +0530

    updates

---
+++ csharp/src/ChatAPI/Logs/AutoFunctionInvocationLoggingFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MultiAgentCopilot.Logs
+{
+    public sealed class AutoFunctionInvocationLoggingFilter : IAutoFunctionInvocationFilter
+    {
+        private readonly ILogger<AutoFunctionInvocationLoggingFilter> _logger;
+
+        public AutoFunctionInvocationLoggingFilter(ILogger<AutoFunctionInvocationLoggingFilter> logger)
+        {
+            _logger = logger;
+
+        }
+
+        public async Task OnAutoFunctionInvocationAsync(AutoFunctionInvocationContext context, Func<AutoFunctionInvocationContext, Task> next)
+        {
+
+            var functionCalls = FunctionCallContent.GetFunctionCalls(context.ChatHistory.Last()).ToList();
+
+            if (_logger.IsEnabled(LogLevel.Trace))
+            {
+                functionCalls.ForEach(functionCall
+                    => _logger.LogTrace(
+                        "Function call requests: {PluginName}-{FunctionName}({Arguments})",
+                        functionCall.PluginName,
+                        functionCall.FunctionName,
+                        JsonSerializer.Serialize(functionCall.Arguments)));
+            }
+
+            await next(context);
+        }
+    }
+}
