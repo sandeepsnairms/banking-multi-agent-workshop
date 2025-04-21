202b582f237dc72caa910f571dc30617d516df59
Sandeep Nair <nair.sandeep@microsoft.com>
2025-04-21 09:38:26 +0530

    initial

---
+++ csharp/src/MultiAgentCopilot/Models/Debug/LogProperty.cs
@@ -0,0 +1,17 @@
+﻿namespace  MultiAgentCopilot.Models.Debug
+{
+    public record LogProperty
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public LogProperty(string key, string value)
+        {
+            Key = key;
+            Value = value;
+            TimeStamp = DateTime.UtcNow;
+
+        }
+    }
+}
