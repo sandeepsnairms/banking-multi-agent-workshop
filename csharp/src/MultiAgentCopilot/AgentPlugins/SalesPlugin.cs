338e4d78ba55877c361278bb130d046c68a97924
Sandeep Nair <nair.sandeep@microsoft.com>
2025-04-25 11:01:03 +0530

    Initial Commit for HOL

---
+++ csharp/src/MultiAgentCopilot/AgentPlugins/SalesPlugin.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+using  MultiAgentCopilot.Models.Banking;
+using MultiAgentCopilot.Services;
+
+namespace MultiAgentCopilot.Plugins
+{
+    internal class SalesPlugin : BasePlugin
+    {
+        public SalesPlugin(ILogger<BasePlugin> logger, BankingDataService bankService, string tenantId, string userId )
+            : base(logger, bankService, tenantId, userId)
+        {
+        }
+               
+
+        [KernelFunction]
+        [Description("Register a new account.")]
+        public async Task<ServiceRequest> RegisterAccount(string userId, AccountType accType, Dictionary<string,string> fulfilmentDetails)
+        {
+            _logger.LogTrace($"Registering Account. User ID: {userId}, Account Type: {accType}");
+            return await _bankService.CreateFulfilmentRequestAsync(_tenantId, string.Empty,_userId,string.Empty,fulfilmentDetails);
+        }
+
+    }
+}
