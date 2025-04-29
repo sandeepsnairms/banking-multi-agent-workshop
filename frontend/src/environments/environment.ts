f6329b0df804ca83d1fff98ad3240647a2e77895
sajeetharan <sajeetharan@outlook.com>
2025-04-29 21:32:29 +0530

    update endpoint

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://ca-webapi-xxz2idhkxrjee.graydune-59d6ac3b.eastus2.azurecontainerapps.io/'
+    apiUrl: 'https://localhost:63279/'
   };
