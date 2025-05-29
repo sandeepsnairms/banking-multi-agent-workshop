67a1ca5cc31ea8568b58027677f7630a7bcfe542
Sandeep Nair <nair.sandeep@microsoft.com>
2025-05-29 10:13:43 +0530

    migration

--- csharp/src/MultiAgentCopilot/Models/Banking/OfferTerm.cs
+++ csharp/src/MultiAgentCopilot/Models/Banking/OfferTerm.cs
@@ -4,8 +4,30 @@ namespace MultiAgentCopilot.Models.Banking
 {
     public class OfferTerm
     {
-       
+
+        [VectorStoreKey]
+        public required string Id { get; set; }
+
+        [VectorStoreData]
+        public required string TenantId { get; set; }
+
+        [VectorStoreData]
+        public required string OfferId { get; set; }
+
+        [VectorStoreData]
+        public required string Name { get; set; }
+
+        [VectorStoreData]
+        public required string Text { get; set; }
+
+        [VectorStoreData]
+        public required string Type { get; set; }
+
+        [VectorStoreData]
+        public required string AccountType { get; set; }
+
+        [VectorStoreVector(Dimensions: 1536, DistanceFunction = DistanceFunction.CosineSimilarity, IndexKind: IndexKind.QuantizedFlat)]
+        public ReadOnlyMemory<float>? Vector { get; set; }
 
     }
-
 }
